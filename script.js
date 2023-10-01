1573662fcd59b1365e4da8da1cc206e73edef0f9
Raf C <130236858+raf-c@users.noreply.github.com>
2023-10-01 19:22:33 +0200

    Add alternative play modes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const container = document.querySelector("#container");
+const container = document.querySelector(".container");
 
 
 for (var i = 0; i < 16; i++) { 
@@ -6,3 +6,36 @@ for (var i = 0; i < 16; i++) {
     result.textContent = `Box ${i+1}`;
     container.appendChild(result);
   }
+
+const grids = document.querySelectorAll('div.container > div') 
+
+
+  grids.forEach((grid) => {
+    grid.addEventListener('mouseover', ()=>{
+      grid.style.backgroundColor = "black";
+    });
+  })
+
+  const reset = document.querySelectorAll('button')
+  reset.addEventListener('click', function(grids){
+    for (grid in grids){
+      console.log("hi");
+      grid.style.backgroundColor = "white";
+    }
+  })
+  
+  //clearBoard(grids));
+
+  function clearBoard(){
+    const grids = document.querySelectorAll('div.container > div') 
+    for (grid in grids){
+      grid.style.backgroundColor = "white";
+    }
+  }
+  
+
+/*function randomColor() {
+  var colors = ['#ff0000', '#00ff00', '#0000ff'];
+  var random_color = colors[Math.floor(Math.random() * colors.length)];
+  return random_color;
+}*/
